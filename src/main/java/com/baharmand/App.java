75ae7d857fff016eafb8957870c0aa54f66a620c
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-08 18:25:28 +0200

    Test the app and constructors

--- src/main/java/com/baharmand/App.java
+++ src/main/java/com/baharmand/App.java
@@ -2,21 +2,15 @@ package com.baharmand;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
-        BankAccount negarAccount = new BankAccount("Negar", "NegarA@gmail.com", "0722915323");
-        negarAccount.printAccount();
-        negarAccount.deposit(1000);
-        negarAccount.withdraw(400);
-        negarAccount.withdraw(700);
+public class App {
+    public static void main(String[] args) {
+        Customer customer1 = new Customer("Negar", "negar@gmail.com", "042356467");
+        BankAccount bankAccount1 = new BankAccount(customer1);
+        bankAccount1.setBalance(100);
+        bankAccount1.printAccount();
+        bankAccount1.deposit(200);
+        bankAccount1.withdraw(150);
 
-        BankAccount erikAccount = new BankAccount("Erik", "Erik@hotmail.com", "0745663325");
-        erikAccount.printAccount();
-
-        BankAccount jointAccount = new BankAccount("Erik & Hannah", "Erik@hotmail.com", "0745663325");
-        jointAccount.printAccount();
     }
 }
